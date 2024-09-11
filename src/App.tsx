78e1d0ab0b2fb6124ccdf65c3e126c0b4358f21c
Shubhamss105 <shubhamsingh.ss.1407@gmail.com>
2024-09-12 00:34:03 +0530

    babel bug fix

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+    <Text className="text-5xl font-bold text-black">Hello, Tailwind css!</Text>
+  </View>
+  );
+};
+
+export default App;
